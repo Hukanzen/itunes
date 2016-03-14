fa9cfeb97c47046b359221a84007b04591209a33
Hukanzen <qazxswed1934@gmail.com>
2016-03-15 08:22:27 +0900

    Second Commit

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-
+use Data::Dumper;
 use XML::Simple;
 $xml=XML::Simple->new();
 
@@ -9,5 +9,18 @@ print "LOAD FILE: $fname \n";
 #Assignment to $xml
 $xml=XMLin($fname);
 
-print ($xml->{dict}->{array}->{dict}[0]->{key}[6]]); #Name
-print ($xml->{dict}->{array}->{dict}[0]->{String}[2]]); #Name - String
+#print ($xml->{dict}->{array}->{dict}[0]->{key}[5]); #Name
+#print "\n";
+#
+#print ($xml->{dict}->{array}->{dict}[0]->{string}[1]); #Name - String
+#print "\n";
+
+print Dumper($xml->{dict}->{array}->{dict}[0]);
+#print Dumper($xml->{dict}->{array}->{dict}[0]->{array});
+
+foreach my $data ($xml->{dict}->{array}->{dict}[0]->{array}->{dict}){
+	print $data->{"Track ID"}->{integer}."\n";
+}
+
+
+print "\n";
