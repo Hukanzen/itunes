8e2985f58b647666e102576db5bd9863d461f7f4
Hukanzen <qazxswed1934@gmail.com>
2016-03-18 23:25:05 +0900

    Third Commit

--- main.pl
+++ main.pl
@@ -1,26 +1,11 @@
 #!/usr/bin/perl
+
 use Data::Dumper;
-use XML::Simple;
-$xml=XML::Simple->new();
+use XML::MyXML;
+use Perl6::Slurp;
 
 $fname="iTunes Library.xml";
 print "LOAD FILE: $fname \n";
 
-#Assignment to $xml
-$xml=XMLin($fname);
+$xml=slurp $fname;
 
-#print ($xml->{dict}->{array}->{dict}[0]->{key}[5]); #Name
-#print "\n";
-#
-#print ($xml->{dict}->{array}->{dict}[0]->{string}[1]); #Name - String
-#print "\n";
-
-print Dumper($xml->{dict}->{array}->{dict}[0]);
-#print Dumper($xml->{dict}->{array}->{dict}[0]->{array});
-
-foreach my $data ($xml->{dict}->{array}->{dict}[0]->{array}->{dict}){
-	print $data->{"Track ID"}->{integer}."\n";
-}
-
-
-print "\n";
