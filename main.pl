0054e2ab5e15f57d28c3e0b2d189cffbb345a738
Hukanzen <qazxswed1934@gmail.com>
2016-03-20 19:21:35 +0900

    4th Commit

--- main.pl
+++ main.pl
@@ -1,11 +1,27 @@
 #!/usr/bin/perl
 
 use Data::Dumper;
-use XML::MyXML;
+use XML::MyXML qw(xml_to_object);
 use Perl6::Slurp;
+
+#=== variable ===#
+$string_vi="s/\Qfile://lcoalhost/G:/\E/\Q/home/Public/\E/g";
 
 $fname="iTunes Library.xml";
 print "LOAD FILE: $fname \n";
 
 $xml=slurp $fname;
 
+$itunes_lib=xml_to_object($xml);
+
+foreach ($itunes_lib->path("dict/dict/key")){
+	foreach $string ($_->path("dict/string")){
+		$p_file_string="m/file:/";
+		if($string->value =~ $p_file_string){
+			last;	
+		}
+	}
+	$string=~$string_vi;
+	print $_->value."->".$string."\n";
+	#@song[$_->value]=$;
+}
