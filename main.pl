93e737ba017b50b6ec9bac14b6dd3dd7dc8de110
Hukanzen <qazxswed1934@gmail.com>
2016-03-23 00:02:35 +0900

    6th Commit

--- main.pl
+++ main.pl
@@ -4,13 +4,14 @@ use Data::Dumper;
 use XML::MyXML qw(xml_to_object);
 use Perl6::Slurp;
 use Encode;
+use URI::Escape;
 
 #=== variable ===#
 $p_file_string=m/^file:/;
 #$p_string_vi_prev=m!file:\/\/localhost\/G:!;
 #$p_string_vi_next=m!\/home\/Public!;
 #$p_string_vi=s/$p_string_vi_prev/$p_string_vi_next/;
-$p_string_vi=s/^file:\/\/localhost\/G:/\/home\/Public/; # $string_visual
+$p_string_vi=s!file://localhost/G:!/home/Public!g; # $string_visual
 #=== variable ===#
 
 #=== load iTunes Lbrary ===#
@@ -36,12 +37,15 @@ foreach($itunes_lib->path("dict/dict/key")){
 $i=0;
 foreach($itunes_lib->path("dict/dict/dict")){
 	@strings=$_->path("string");
+	$path="no file";
 	foreach my $string(@strings){
 		if($string->value =~ $p_file_string){
 #			print $string->value."\n";
 			$path=$string->value;
-			$path=~$p_string_vi;
 			$path=encode("utf-8",$path);
+			$path=uri_unescape($path);
+#			$path=~$p_string_vi;
+			$path =~ s!^file://localhost!!;
 			@music_file[$i]=$path;
 		}
 	}
